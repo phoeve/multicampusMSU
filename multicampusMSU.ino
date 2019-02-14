573471c8d8da32b34620f4ec656b1e1f715108e4
Peter Hoeve <peter@hoeve.us>
2019-02-13 21:59:42 -0500

    Vastest working version

--- multicampusMSU.ino
+++ multicampusMSU.ino
@@ -4,7 +4,11 @@
 
 //  You can turn console messages off to improve performance
 #define CONSOLE 1
+#define DEBUG_BUTTONS 0
 
+#define LOCAL   01
+#define LRC     10
+#define GVG7000 11
 struct route
 {
   byte            button;
@@ -14,29 +18,37 @@ struct route
   byte            platinumOut;
   byte            ultirxIp[4];
   unsigned int    ultirxPort;  
+  byte            remote_protocol;
 } Buttons[] = {
-                      {  1,   0,   0,  33,  26, 0, 0 },   // NPCC
-                      {  2,   0,   0,  34,  26, 0, 0 },
-                      {  3,   0,   0,  35,  26, 0, 0 },
-                      {  4,   0,   0,  39,  26, 0, 0 },
-                      {  5,   0,   0,  40,  26, 0, 0 },
-                      {  6,   0,   0,  43,  26, 0, 0 },
-                      {  7,   0,   0,  44,  26, 0, 0 },
-                      {  8,   0,   0,  45,  26, 0, 0 },
-                      { 10,   0,   0,  42,  26, 0, 0 },
+                {  1,   0,   0,  33,  26, 0, 0, LOCAL },   // NPCC
+                {  2,   0,   0,  34,  26, 0, 0, LOCAL },
+                {  3,   0,   0,  35,  26, 0, 0, LOCAL },
+                {  4,   0,   0,  39,  26, 0, 0, LOCAL },
+                {  5,   0,   0,  40,  26, 0, 0, LOCAL },
+                {  6,   0,   0,  43,  26, 0, 0, LOCAL },
+                {  7,   0,   0,  44,  26, 0, 0, LOCAL },
+                {  8,   0,   0,  45,  26, 0, 0, LOCAL },
+                { 10,   0,   0,  42,  26, 0, 0, LOCAL },
 
-                      { 11,  32,  26, 112,  26, 172,17,3,137, 12345 },   // BC 8910 or 12345
-                      { 12,  33,  26, 112,  26, 172,17,3,137, 12345 },
-                      { 13,  34,  26, 112,  26, 172,17,3,137, 12345 },
-                      { 14,  38,  26, 112,  26, 172,17,3,137, 12345 },
-                      { 15,  39,  26, 112,  26, 172,17,3,137, 12345 },
-                      { 20,  62,  26, 112,  26, 172,17,3,137, 12345 },  // 78 +48 +84 +73 +9 +63 +9 +27 
-                      
-                      {0,0,0,0,0,0,0,0,0,0}   // Table termintor
+                { 11,  32,  26, 112,  26, 172,17,3,137, 12345, GVG7000 },   // BC
+                { 12,  33,  26, 112,  26, 172,17,3,137, 12345, GVG7000 },
+                { 13,  34,  26, 112,  26, 172,17,3,137, 12345, GVG7000 },
+                { 14,  38,  26, 112,  26, 172,17,3,137, 12345, GVG7000 },
+                { 15,  39,  26, 112,  26, 172,17,3,137, 12345, GVG7000 },
+                { 20,  62,  26, 112,  26, 172,17,3,137, 12345, GVG7000 },   
+
+                { 11,  32,  26, 112,  26, 172,17,3,137, 12345, LRC },   // BB
+                { 12,  33,  26, 112,  26, 172,17,3,137, 12345, LRC },
+                { 13,  34,  26, 112,  26, 172,17,3,137, 12345, LRC },
+                { 14,  38,  26, 112,  26, 172,17,3,137, 12345, LRC },
+                { 15,  39,  26, 112,  26, 172,17,3,137, 12345, LRC },
+                { 20,  62,  26, 112,  26, 172,17,3,137, 12345, LRC },  
+                
+                {0,0,0,0,0,0,0,0,0,0}   // Table termintor
 };
 
-byte platinumIp[] = {172,26,3,161};
-unsigned int platinumPort = 52116;
+byte          platinumIp[] = {172,26,3,161};    // Local NPCC router
+unsigned int  platinumPort = 52116;
 
 
 #define GPI_PIN_ON    HIGH             // If voltage is HIGH, the dip switch is in the ON position. 
@@ -45,9 +57,6 @@ unsigned int platinumPort = 52116;
 
 // MAC address 
 byte MyMac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-
-
-
 IPAddress MyIP(172, 16, 3, 183);
 // the dns server ip
 IPAddress dnsServer(10, 16, 16, 10);
@@ -56,22 +65,22 @@ IPAddress gateway(172, 16, 1, 254);
 // the subnet:
 IPAddress subnet(255, 255, 0, 0);
 
+
+
 void setup() {
   // start the Ethernet connection:
   Ethernet.begin(MyMac, MyIP, dnsServer, gateway, subnet);
 
-
 #if CONSOLE
   // Open serial communications and wait for port to open:
   Serial.begin(57600);
-
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 #endif
 
   // give the Ethernet shield a second to initialize
-  //delay(1000);
+  delay(1000);
 
 }
 
@@ -93,60 +102,54 @@ void loop() {
     digitalWrite(pin, HIGH);       // turn on pullup resistor
     
     val = digitalRead(pin);
-    //Serial.print(val);
+    
+#if DEBUG_BUTTONS
+    Serial.print(val);
+#endif
+
     if (val==GPI_PIN_ON){
       bitSet(button, pin-2);  
     } 
   }
   button++;
 
-//button = 20;
-
-//  Serial.print(" ==> ");
-
-  if ((last_button != button)){      // Don't repeat if we received an ack on same request
+  if ((last_button != button)){      // Don't repeat (button stays pushed)
     int row;
-
-//    Serial.print("---------------------------------\nbutton=");
-//    Serial.println(button);
     
     row = button2row(button);
-//    Serial.print("row=");
-//    Serial.print(row);
-//    Serial.println("");
 
+#if DEBUG_BUTTONS
+    Serial.print("---------------------------------\nbutton=");
+    Serial.println(button);    
+    Serial.print("row=");
+    Serial.print(row);
+    Serial.println("");
+#endif
+    
     if (row == -1){        // button number out of bounds.
       delay(300);
       return;
     }
+    
+    switch (Buttons[row].remote_protocol){            // Switch to desired source on remote router
+      
+      case GVG7000:                                   // SWP-08/GVG7000 only if if row has info
+        got_ack = send7000Packet (Buttons[row].ultirxIp, Buttons[row].ultirxPort, Buttons[row].ultirxIn, Buttons[row].ultirxOut);
+        break;
 
-
-
-    if (Buttons[row].ultirxIn){           // SWP-08 only if if row has info
-      got_ack = send7000Packet (Buttons[row].ultirxIp, Buttons[row].ultirxPort, Buttons[row].ultirxIn, Buttons[row].ultirxOut);
-      //got_ack = sendLRCPacket (Buttons[row].ultirxIp, Buttons[row].ultirxPort, Buttons[row].ultirxIn, Buttons[row].ultirxOut);
-
+      case LRC:                                       // Send LRC packet
+        got_ack = sendLRCPacket (Buttons[row].ultirxIp, Buttons[row].ultirxPort, Buttons[row].ultirxIn, Buttons[row].ultirxOut);
+        break;
     }
-
+                            // Switch local NPCC router to show remote's router.
     got_ack = sendLRCPacket (platinumIp, platinumPort, Buttons[row].platinumIn, Buttons[row].platinumOut);
-
-
-  }
   
-  last_button = button;     // Only send message once.
+    last_button = button;     // Only send message once.
+    
+  } // end if ((last_button != button)){ 
   
 }
 
-int button2row(byte button)
-{
-  int i;
-  
-  for (i=0; Buttons[i].button!=0; i++)
-    if(button == Buttons[i].button)
-      return i;
-
-  return -1;
-}
 
 //  ~XPOINT:S${SAT 1};D${MON 6}\
 
@@ -179,29 +182,15 @@ boolean sendLRCPacket(IPAddress ip, unsigned int port, byte src, byte dest){
     }
   }
 
-
   client.println(msg);
   Serial.println(" ... sent.\n\n");
-  //client.stop();
 
-//    for (int i=0; i<3; i++){
-//      while (client.available()) {
-//        char c = client.read();
-//        Serial.print(c);
-//      }
-//      if (false)
-//        break;
-//      delay(1000);       // wait 0.1 sec
-//    }
-//  Serial.println(" ...bailing");
-//  client.stop();
   return true;
     
 }
 
 
 boolean send7000Packet(IPAddress ip, unsigned int port, byte src, byte dest){
-
 
   static EthernetClient client;
   static IPAddress connectedIP(0,0,0,0);
@@ -210,19 +199,17 @@ boolean send7000Packet(IPAddress ip, unsigned int port, byte src, byte dest){
   unsigned int sum=0;
   unsigned int cksum;
 
-  sprintf(msg, "N0TI\t%02x\t%02x", dest, src);
-  //sprintf(msg, "N0QI\t%04x", dest);
+  sprintf(msg, "N0TI\t%02x\t%02x", dest, src);  // Basic 7000 message w/o checksum and framing
 
   for (int i=0; i< strlen(msg); i++){
     sum += msg[i];
-
     sprintf(str, "%d/%x ", msg[i], msg[i]);
     Serial.print(str);
   }
   
   cksum = 0x100 - (sum % 0x100);
 
-  sprintf(str, " = %d/%x, cksum = %d/%x", sum,sum, cksum,cksum);
+  sprintf(str, " = %d/%x, cksum = %d/%x", sum,sum, cksum,cksum);    // Add checksum
   Serial.println(str);
 
   sprintf (&msg[strlen(msg)], "%02x", cksum);   // concat ascii of hex cksum 
@@ -233,19 +220,12 @@ boolean send7000Packet(IPAddress ip, unsigned int port, byte src, byte dest){
   if (client.connect(ip, port)) {
     Serial.println("... connected ...");
     Serial.write(msg, strlen(msg));
-    client.write(0x01);                       // SOH
+    
+    client.write(0x01);                       // SOH  Send framing
     client.write(msg, strlen(msg));
     client.write(0x04);                       // EOT
-    Serial.println(" ... sent.\n\n");
     
-//    for (int i=0; i<3; i++){
-//      while (client.available()) {
-//        char c = client.read();
-//        Serial.print(c);
-//      }
-//      delay(1000);       // wait 0.1 sec
-//    }
-//    Serial.println(" ...bailing on ACK.");
+    Serial.println(" ... sent.\n\n");
     
     client.stop();
   }
@@ -256,9 +236,19 @@ boolean send7000Packet(IPAddress ip, unsigned int port, byte src, byte dest){
   }
   
   return true;
+  
 }
 
+int button2row(byte button)
+{
+  int i;
+  
+  for (i=0; Buttons[i].button!=0; i++)
+    if(button == Buttons[i].button)
+      return i;
 
+  return -1;    // Button not in table, dude !
+}
 
 boolean sameIP(IPAddress ip1, IPAddress ip2)
 {
